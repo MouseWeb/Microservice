7653eda0c030554bd9792b29ef78beec985e3d3c
Douglas <douglas@mouseweb.com.br>
2021-01-31 23:37:53 -0300

    criado o servidor de configuração

---
+++ config-server/src/test/java/br/com/mouseweb/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mouseweb.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
